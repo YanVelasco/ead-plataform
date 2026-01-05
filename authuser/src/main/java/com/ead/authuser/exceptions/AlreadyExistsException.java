47334302540c591efa01f7c5397bbcf5097b7503
Yan Velasco <yancarlosduartevelasco@gmail.com>
2026-01-05 17:46:19 -0300

    add custom exceptions for error handling; implement DatabaseException, InvalidImageException, and ValidationException; update GlobalExceptionHandler for new exception handling; refactor UserService and UserServiceImpl for image processing

--- authuser/src/main/java/com/ead/authuser/exceptions/AlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.ead.authuser.exceptions;
-
-public class AlreadyExistsException extends RuntimeException {
-    public AlreadyExistsException(String message) {
-        super(message);
-    }
-}
