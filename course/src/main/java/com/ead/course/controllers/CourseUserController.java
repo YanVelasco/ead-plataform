980abbb4f8591b4b3ab6a7057f9ba7a77eb09ec9
YanVelasco <yancarlosduartevelasco@gmail.com>
2026-02-25 15:01:27 -0300

    update getUserByCourseId method in CourseUserController to use default sorting for pageable requests

--- course/src/main/java/com/ead/course/controllers/CourseUserController.java
+++ course/src/main/java/com/ead/course/controllers/CourseUserController.java
@@ -5,6 +5,8 @@ import com.ead.course.dtos.PageDto;
 import com.ead.course.dtos.UserDto;
 import com.ead.course.services.CourseUserService;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,7 +28,7 @@ public class CourseUserController {
     @GetMapping("/cousers/{courseId}/users")
     public ResponseEntity<PageDto<UserDto>> getUserByCourseId(
             @PathVariable UUID courseId,
-            Pageable pageable
+            @PageableDefault(sort = "userId", direction = Sort.Direction.ASC) Pageable pageable
     ) {
         return ResponseEntity.ok(authUserClient.getAllUsersByCourse(courseId, pageable));
     }
